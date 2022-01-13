112f1474e6a0067b4d780270a1d13458aabb695d
Mykola Sopiha <mykola_sopiha@epam.com>
2022-01-13 21:54:12 +0200

    events

---
+++ events.js
@@ -0,0 +1,26 @@
+((EventEmitter) => {
+  const ee = new EventEmitter();
+
+  /**
+   * Add logging from `ee.on(task, cb)` method.
+   * Log task and cb in console
+   */
+  
+  //...your code here...
+
+  ee.on("foo", (data) => console.log(data));
+  ee.on("bar", (data) => console.log(data));
+
+  ee.emit("foo", 111);
+  ee.emit("bar", 222);
+})(require("events"));
+
+
+/**
+ * Answer:
+ */
+// const oldOn = ee.on;
+// ee.on = function (task, cb) {
+//   console.log(task, cb);
+//   oldOn.apply(this, [task, cb]);
+// };
